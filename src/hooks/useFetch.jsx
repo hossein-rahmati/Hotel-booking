b1f455d98d9dfdb73b1a40f055e9b639eb9e5a0c
hossein rahmati <rmthossein@gmail.com>
2023-10-06 14:20:48 +0330

    develop LocationList component

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -10,7 +10,7 @@ export default function useFetch(url, query = "") {
     async function fetchData() {
       try {
         setIsLoading(true);
-        const { data } = axios.get(`${url}?${query}`);
+        const { data } = await axios.get(`${url}?${query}`);
         setData(data);
       } catch (error) {
         setData([]);
